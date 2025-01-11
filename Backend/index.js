dfa0855b08f12fda9c98bb8cd8afe7c71453d603
charan <cherrytan2003@gmail.com>
2025-01-11 22:57:31 +0530

    Connect to MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+let url=process.env.MONGO;
+
+mongoose
+.connect(url)
+.then(()=>{
+    console.log('Connected to MONGODB');
+})
+.catch((err)=>{
+    console.log("Error ",err);
+})
+
+
 const app=express();
+
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 });
