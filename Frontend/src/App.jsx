354c3dca4df36499b11104d6a3b3d37cbd5149df
charan <cherrytan2003@gmail.com>
2025-01-11 16:37:35 +0530

    Created Pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './Pages/Home.jsx';
+import About from './Pages/About.jsx'
+import Signin from './Pages/Signin.jsx'
+import Contact from './Pages/Contact.jsx'
+import Signup from './Pages/Signup.jsx';
+import Explore from './Pages/Explore.jsx';
 
 export default function App() {
   return (
-    <h1 className='text-3xl'>app</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<Signin/>}/>
+        <Route path='/sign-up' element={<Signup/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/explore' element={<Explore/>}/>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
