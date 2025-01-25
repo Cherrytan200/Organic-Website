288cefc7da2d901de9aea9d78b6d0506615779cd
charan <cherrytan2003@gmail.com>
2025-01-25 21:52:55 +0530

    Sidebar

--- Frontend/src/layout/MainLayout.jsx
+++ Frontend/src/layout/MainLayout.jsx
@@ -1,7 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import Header from './Header.jsx';
+import Sidebar from './Sidebar.jsx';
 
 export default function MainLayout() {
   return (
-    <div>Main Layout <Outlet/></div>
+    <div className='bg-[#cdcae9] w-full min-h-screen'>
+      <Header/>
+      <Sidebar/>
+      <div className='ml-0 lg:ml-[260px] pt-[95px] transition-all'>
+        <Outlet/>
+      </div>
+     </div>
   )
 }
